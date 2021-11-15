a53a3df2a330608e7a1f4c4a6de46c0c963aa271
Luis Serrano <84140881+LuisBy860@users.noreply.github.com>
2021-11-15 15:00:18 -0600

    implemenrtado opciones de crud

--- ZapateriaJossielito/Repository/EstilosRepository.cs
+++ ZapateriaJossielito/Repository/EstilosRepository.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ZapateriaJossielito.Models;
+using ZapateriaJossielito.Service;
 
 namespace ZapateriaJossielito.Repository
 {
-    public class EstilosRepository
+    public class EstilosRepository : IEstilos
     {
-    }
+        ZapateriaJossielEntities bd = new ZapateriaJossielEntities();
+        public void Create(Estilos c)
+        {
+            bd.Estilos.Add(c);
+            bd.SaveChanges();
+        }
+
+        public void Delete(Estilos c)
+        {
+            c = bd.Estilos.Find(c.IdEstilo);
+            _ = c;
+            bd.Estilos.Remove(c);
+            bd.SaveChanges();
+        }
+
+        public List<Estilos> ListDataEstilos()
+        {
+            var ListOfDataOfEstilo = bd.Estilos.ToList();
+
+            return ListOfDataOfEstilo;
+        }
+
+        public void Update(Estilos c)
+        {
+            Estilos actualizar = new Estilos();
+            actualizar = bd.Estilos.Find(c.IdEstilo);
+            actualizar.Nombre = c.Nombre;
+            bd.SaveChanges();
+        }
+    } 
 }
--- ZapateriaJossielito/Service/IEstilos.cs
+++ ZapateriaJossielito/Service/IEstilos.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZapateriaJossielito.Models;
 
 namespace ZapateriaJossielito.Service
 {
     interface IEstilos
     {
+
+
+        void Create(Estilos c);
+
+        void Update(Estilos c);
+
+        void Delete(Estilos c);
+
+        List<Estilos> ListDataEstilos();
     }
 }
