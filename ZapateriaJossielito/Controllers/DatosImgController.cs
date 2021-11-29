1d949f871095ccfaf74c479e0c2c10a64cd000b8
Jose101503 <ph0172032019@unab.edu.sv>
2021-11-28 19:20:59 -0600

    errores de controlador img reparado

--- ZapateriaJossielito/Controllers/DatosImgController.cs
+++ ZapateriaJossielito/Controllers/DatosImgController.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZapateriaJossielito.Models;
 
 namespace ZapateriaJossielito.Controllers
 {
     public class DatosImgController : Controller
     {
         // GET: DatosImg
-        public ActionResult Index()
+
+       
+        [HttpGet]
+        public ActionResult AgregarIMG()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Imagen imagen)
+        {
+            _ = imagen.file;
+            String ruta = Server.MapPath("~/Img/");
+            ruta += imagen.file.FileName;
+            imagen.file.SaveAs(ruta);
+            return View();
+        }
+        public ActionResult Second()
         {
             return View();
         }
